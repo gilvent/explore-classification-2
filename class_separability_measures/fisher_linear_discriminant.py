dd69eb818c72586a9d3043301f560fdaefba11f0
Alvaro <alvarolukmanto@gmail.com>
2025-04-30 17:15:12 +0800

    feat(FLD): calculate separability after projection to 1d space

--- class_separability_measures/fisher_linear_discriminant.py
+++ class_separability_measures/fisher_linear_discriminant.py
@@ -17,7 +17,7 @@ def projection_vector(within_cl_scatter, X_cl_1: np.ndarray, X_cl_0: np.ndarray)
 
 # Using Fisher's criterion formula
 # J(w) = (w_transpose . (Sb . w)) / (w_transpose . (Sw . w))
-def class_separability(projection_vector: np.ndarray, within_cl_scatter: np.ndarray, between_cl_scatter: np.ndarray):
+def fdr(projection_vector: np.ndarray, within_cl_scatter: np.ndarray, between_cl_scatter: np.ndarray):
     w = projection_vector
     Sw = within_cl_scatter
     Sb = between_cl_scatter
@@ -26,3 +26,24 @@ def class_separability(projection_vector: np.ndarray, within_cl_scatter: np.ndar
     denominator = w.T @ (Sw @ w)
 
     return (numerator / denominator).item()
+
+
+def fdr_1d(X_cl_1_projected: np.ndarray, X_cl_0_projected: np.ndarray):
+    cl_1_projected_mean = np.mean(X_cl_1_projected, axis=0)
+    cl_0_projected_mean = np.mean(X_cl_0_projected, axis=0)
+
+    # Calculate class 1 variance
+    cl_1_samples = X_cl_1_projected.shape[0]
+    cl_1_mean_diff_matrix = X_cl_1_projected - cl_1_projected_mean
+    # Variance = Sum of (projected x - projected x mean)^2 / number of samples
+    cl_1_variance = np.sum(np.power(cl_1_mean_diff_matrix, 2)) / cl_1_samples
+    
+
+    # Calculate class 0 variance
+    cl_0_samples = X_cl_0_projected.shape[0]
+    cl_0_mean_diff_matrix = X_cl_0_projected - cl_0_projected_mean
+    # Variance = Sum of (projected x - projected x mean)^2 / number of samples
+    cl_0_variance = np.sum(np.power(cl_0_mean_diff_matrix, 2)) / cl_0_samples
+
+    fdr =  np.power(cl_1_projected_mean - cl_0_projected_mean, 2) / (cl_0_variance + cl_1_variance)
+    return fdr.item()
--- experiments/heart_disease/fisher_linear_discriminant.py
+++ experiments/heart_disease/fisher_linear_discriminant.py
@@ -1,7 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from class_separability_measures.scatter_matrix import within_class_scatter_matrix, between_class_scatter_matrix
-from class_separability_measures.fisher_linear_discriminant import projection_vector, class_separability
+from class_separability_measures.scatter_matrix import (
+    within_class_scatter_matrix,
+    between_class_scatter_matrix,
+)
+from class_separability_measures.fisher_linear_discriminant import (
+    projection_vector,
+    fdr,
+    fdr_1d,
+)
 
 
 def main():
@@ -20,14 +27,30 @@ def main():
 
     Sw = within_class_scatter_matrix(X, Y)
     Sb = between_class_scatter_matrix(X, Y)
+    X_cl_1 = X[Y == 1]
+    X_cl_0 = X[Y == 0]
 
     # Get the projection vector
-    w = projection_vector(within_cl_scatter=Sw, X_cl_1=X[Y == 1], X_cl_0=X[Y == 0])
+    w = projection_vector(within_cl_scatter=Sw, X_cl_1=X_cl_1, X_cl_0=X_cl_0)
 
     # Compute FLD before projection
-    sep_before_projecton = class_separability(projection_vector=w, within_cl_scatter=Sw, between_cl_scatter=Sb)
+    sep_before_projecton = fdr(
+        projection_vector=w, within_cl_scatter=Sw, between_cl_scatter=Sb
+    )
 
-    print(f"Separability before projection: {sep_before_projecton:.3f}" )
+    print(f"Separability before projection: {sep_before_projecton:.3f}")
+
+    # Project the data onto the discriminant
+    X_projected = X @ w
+    X_cl_1_projected = X_projected[Y == 1]
+    X_cl_0_projected = X_projected[Y == 0]
+
+    # Calculate separability on the projection
+    sep_after_projection = fdr_1d(
+        X_cl_1_projected=X_cl_1_projected, X_cl_0_projected=X_cl_0_projected
+    )
+
+    print(f"Separability after projection: {sep_after_projection:.3f}")
 
 
 
