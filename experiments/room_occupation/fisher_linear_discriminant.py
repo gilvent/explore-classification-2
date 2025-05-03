2671aadd92463afe2a1a207345cf2fa50f2ab9ff
Alvaro <alvarolukmanto@gmail.com>
2025-05-03 16:08:23 +0800

    experiment(room_occupancy_dataset): apply FLD to room_occupancy dataset

---
+++ experiments/room_occupation/fisher_linear_discriminant.py
@@ -0,0 +1,122 @@
+import numpy as np
+from evaluation.metrics import accuracy_score, recall_score, precision_score, f1_score
+from evaluation.confusion_matrix import confusion_matrix, display_confusion_matrix
+from utils.data_preprocess import (
+    to_seconds_since_midnight,
+)
+from evaluation.roc_curve import print_roc_curve, predictions_by_threshold
+from dimensionality_reduction.class_separability_measures import (
+    fld_vector,
+    trace_ratio,
+)
+from visualization.fld import display_1d_projection
+
+
+def preprocess(dataset):
+    # Convert the second column (date time string) into numerical value
+    X_2nd_col = dataset[:, 1]
+    updated_X_2nd_col = []
+
+    for datestr in X_2nd_col:
+        time_in_seconds = to_seconds_since_midnight(datestr.replace('"', ""))
+        updated_X_2nd_col.append(time_in_seconds)
+
+    updated_X_2nd_col = np.array(updated_X_2nd_col)
+
+    # Combine the updated second column with the rest, we don't use the first column
+    X_rest = dataset[:, 2:-1].astype(float)
+    X = np.hstack((updated_X_2nd_col.reshape(-1, 1), X_rest))
+    Y = dataset[:, -1].astype(float)
+
+    return (X, Y)
+
+
+def main():
+    training_set = np.loadtxt(
+        fname="data/room_occupancy_datatraining.txt", delimiter=",", dtype=str
+    )
+    test_set = np.loadtxt(
+        fname="data/room_occupancy_datatest.txt", delimiter=",", dtype=str
+    )
+
+    train_X, train_Y = preprocess(training_set)
+    test_X, test_Y = preprocess(test_set)
+
+    classes = np.unique(train_Y)
+
+    # Separability before projection
+    initial_class_separability = trace_ratio(X=train_X, Y=train_Y)
+
+    # Get the projection vector
+    w = fld_vector(X=train_X, Y=train_Y)
+
+    # Project the data onto the discriminant
+    X_projected = train_X @ w
+
+    # Separability after projection
+    projection_class_separability = trace_ratio(X=X_projected, Y=train_Y)
+
+    display_1d_projection(
+        X_projected=X_projected,
+        Y=train_Y,
+        title=f"Class separability, initial: {initial_class_separability:.3f}, projection: {projection_class_separability:.3f}",
+    )
+
+    # Make predictions based on 1d projection
+    test_X_projected = test_X @ w
+
+    test_X_1_projected_mean = np.mean(test_X_projected[test_Y == 1])
+    test_X_0_projected_mean = np.mean(test_X_projected[test_Y == 0])
+    threshold = (test_X_1_projected_mean + test_X_0_projected_mean) / 2
+    pred_Y = predictions_by_threshold(test_X_projected, threshold)
+
+    # Confusion matrix
+    conf_matrix = confusion_matrix(classes=classes, actual_Y=test_Y, pred_Y=pred_Y)
+    accuracy = accuracy_score(actual_Y=test_Y, pred_Y=pred_Y)
+    recall = recall_score(tp=conf_matrix[1][1], fn=conf_matrix[1][0])
+    precision = precision_score(tp=conf_matrix[1][1], fp=conf_matrix[0][1])
+    f1 = f1_score(recall=recall, precision=precision)
+    info_text = f"Accuracy: {accuracy:.2f}, F1 Score: {f1:.2f}"
+
+    display_confusion_matrix(
+        conf_matrix=conf_matrix,
+        classes=classes,
+        title="Room occupancy/Fisher Linear Discriminant",
+        info=info_text,
+    )
+
+    # ROC Curve
+    tpr = []
+    fpr = []
+
+    
+
+    # Get range of thresholds for classification
+    lowest_X_projected = np.min(X_projected)
+    highest_X_projected = np.max(X_projected)
+
+    # Create 10 thresholds for ROC curve
+    step = (highest_X_projected - lowest_X_projected) / 10
+    thresholds = np.arange(lowest_X_projected, highest_X_projected + step + 0.1, step)
+
+    for t in thresholds:
+        thresholded_pred = predictions_by_threshold(
+            discriminants=test_X_projected, threshold=t
+        )
+        conf_matrix = confusion_matrix(
+            classes=classes, actual_Y=test_Y, pred_Y=thresholded_pred
+        )
+
+        tp = conf_matrix[1][1]
+        fp = conf_matrix[0][1]
+        tn = conf_matrix[0][0]
+        fn = conf_matrix[1][0]
+
+        tpr.append(tp / (tp + fn))
+        fpr.append(fp / (fp + tn))
+
+    print_roc_curve(fpr, tpr)
+
+
+if __name__ == "__main__":
+    main()
